a872489eb3051e9061d49f10cbfbf0302f5384a8
voplotitel <antonkultaew@gmail.com>
2023-08-25 11:26:59 +0200

    new job

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
+import { useState } from 'react';
+import { data } from './data';
 import './App.css';
 
 function App() {
+
+  const [musicTool, setMusicTool] = useState(0);
+  const { id, tool, description, image } = data[musicTool];
+
+  const previosMusicTool = () => {
+    setMusicTool((musicTool) => {
+      musicTool--;
+      if (musicTool < 0) {
+        return data.length - 1;
+      }
+      return musicTool;
+    });
+  }
+
+  const nextMusicTool = () => {
+    setMusicTool((musicTool) => {
+      musicTool++;
+      if (musicTool > data.length - 1) {
+        musicTool = 0;
+      }
+      return musicTool;
+    });
+  }
+
+  const removeMusic = (idToRemove) => {
+    const newData = data.filter((musicItem) => musicItem.id !== idToRemove);
+    setMusicTool(0);
+    data.splice(0, data.length, ...newData);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className='container'>
+        <img src={image} alt={tool} width="300px" />
+      </div>
+      <div className='container'>
+        <h1>{id} - {tool} </h1>
+      </div>
+      <div className='container'>
+        <h3>{description}</h3>
+      </div>
+      <div className="btn container">
+        <button onClick={previosMusicTool}>Previos</button>
+        <button onClick={() => removeMusic(id)}>Remove</button>
+        <button onClick={nextMusicTool}>Next</button>
+      </div>
     </div>
   );
 }
---
+++ src/data.js
@@ -0,0 +1,36 @@
+export const data = [
+    {
+        id: 1,
+        tool: "Guitar",
+        image: "https://images.unsplash.com/photo-1550291652-6ea9114a47b1?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=687&q=80",
+        description: "The guitar is a fretted musical instrument that typically has six strings. It is usually held flat against the player's body and played by strumming or plucking the strings with the dominant hand, while simultaneously pressing selected strings against frets with the fingers of the opposite hand. A plectrum or individual finger picks may also be used to strike the strings."
+    },
+
+    {
+        id: 2,
+        tool: "Saxophone",
+        image: "https://images.unsplash.com/photo-1655891182838-bbb0596a1d2d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=687&q=80",
+        description: "The saxophone (often referred to colloquially as the sax) is a type of single-reed woodwind instrument with a conical body, usually made of brass. As with all single-reed instruments, sound is produced when a reed on a mouthpiece vibrates to produce a sound wave inside the instrument's body."
+    },
+
+    {
+        id: 3,
+        tool: "Drum",
+        image: "https://images.unsplash.com/photo-1563845104282-efeedd2b75d8?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=687&q=80",
+        description: "The drum is a member of the percussion group of musical instruments. In the Hornbostel-Sachs classification system, it is a membranophone.[1] Drums consist of at least one membrane, called a drumhead or drum skin, that is stretched over a shell and struck, either directly with the player's hands, or with a percussion mallet, to produce sound."
+    },
+
+    {
+        id: 4,
+        tool: "Piano",
+        image: "https://images.unsplash.com/photo-1615887499117-0f2e95cadbf7?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=687&q=80",
+        description: "The piano is a keyboard instrument that produces sound when pressed on the keys. Most modern pianos have a row of 88 black and white keys: 52 white keys for the notes of the C major scale (C, D, E, F, G, A and B) and 36 shorter black keys raised above the white keys and set further back, for sharps and flats. This means that the piano can play 88 different pitches (or notes), spanning a range of a bit over seven octaves. The black keys are for the accidentals (F♯/G♭, G♯/A♭, A♯/B♭, C♯/D♭, and D♯/E♭), which are needed to play in all twelve keys."
+    },
+
+    {
+        id: 5,
+        tool: "harp",
+        image: "https://images.unsplash.com/photo-1601902186937-b6c743ae2cd3?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=687&q=80",
+        description: "The harp is a stringed musical instrument that has individual strings running at an angle to its soundboard; the strings are plucked with the fingers. Harps can be made and played in various ways, standing or sitting, and in orchestras or concerts. Its most common form is triangular in shape and made of wood. Some have multiple rows of strings and pedal attachments."
+    }
+]
